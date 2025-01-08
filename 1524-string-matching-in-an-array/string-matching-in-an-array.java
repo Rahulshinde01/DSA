50446f777678f24d890674dbc17448df4486fd81
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-08 13:08:22 +0530

    Time: 6 ms (20.93%) | Memory: 42.4 MB (26.85%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,43 +1,43 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        List<String> res = new ArrayList<>();
-        int n = words.length;
-        for (int i = 0;i < n;i++) {
 
-            for (int j = 0;j < n;j++) {
-                if (i == j || words[i].length() > words[j].length()) continue;
+        int len = words.length;
 
-                if (isSubstring( words[i], words[j] )) {
-                    res.add(words[i]);
-                    break; 
+        List<String> ans = new ArrayList<>();
+
+        for(int i = 0; i < len; i++){
+            String str = words[i];
+            int strLen = str.length();
+
+            for(int j = 0; j < len; j++){
+                String word = words[j];
+                int wordLen = word.length();
+
+                if(j == i || strLen > wordLen){
+                    continue;
+                }
+
+                boolean flag = false;
+                for(int k = 0; k < wordLen; k++){
+                    int m = 0; // for str
+                    int n = k; // for word
+                    while((m < strLen) && (n < wordLen) && (word.charAt(n) == str.charAt(m))){
+                        n++;
+                        m++;
+                    }
+                    if(m == strLen){
+                        flag = true;
+                        ans.add(str);
+                        break;
+                    }
+                }
+
+                if(flag){
+                    break;
                 }
             }
-
         }
-
-        return res;
-    }
-
-
-    public boolean isSubstring(String sub, String word) {
-        int n = word.length();
-        int m = sub.length();
-        for (int j = 0; j< n; j++) {
-            boolean isMatch = true;
-            int k=j;
-            int i=0;
-            while(i<m){
-                if (k >= word.length() || word.charAt(k) != sub.charAt(i)) {
-                    isMatch = false;
-                    break; 
-                }
-                k++;
-                i++;
-            }
-            if (isMatch) {
-                return true; 
-            }
-        }
-        return false;
+        return ans;
+        
     }
 }
