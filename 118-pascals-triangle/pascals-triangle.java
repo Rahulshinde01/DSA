deb30b453cd104c15a749ee75392695fd6526f85
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-09 13:42:31 +0530

    Time: 1 ms (85.86%) | Memory: 42.1 MB (51.15%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,32 +1,27 @@
 class Solution {
+
+     // recusive solution
+     
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-
-        // for numRow == 1 
-        list.add(1);
-        ans.add(list);
-
-        for(int i = 1; i < numRows; i++){
-            list = new ArrayList<>(i + 1);
-            int j = 0;
-
-            while(j <= i){ 
-                if(j == 0 || j == i){
-                    list.add(1);
-                }else{
-                    int first = ans.get(i - 1).get(j - 1);
-                    int sec = ans.get(i - 1).get(j);
-
-                    list.add(first + sec);
-                }
-                j++;
-            }
-
-            ans.add(list);
-            
+        if (numRows == 0) return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
         }
-
-        return ans;
+        
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+        
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+        
+        prevRows.add(newRow);
+        return prevRows;
     }
 }
