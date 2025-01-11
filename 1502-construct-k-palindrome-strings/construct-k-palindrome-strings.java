8609309f4012fce293ecbfaa41f903e6cee0c1ef
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-11 17:52:55 +0530

    Time: 4 ms (94.60%) | Memory: 45.4 MB (45.32%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        int len = s.length();
+        if(k > len){
+            return false;
+        }
+        if(k == len){
+            return true;
+        }
+
+        int[] arr = new int[26];
+
+        for(char ch : s.toCharArray()){
+            arr[ch - 'a']++;
+        }
+
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(arr[i] % 2 != 0){
+                count++;
+            }
+        }
+
+        if(count <= k){
+            return true;
+        }
+        
+        return false;
+       
+    }
+}
