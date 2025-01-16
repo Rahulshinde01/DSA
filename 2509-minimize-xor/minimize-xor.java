3ef581a9926be12484fb48719bbe711fe31f1cdf
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-16 09:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (47.22%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,40 +1,43 @@
 class Solution {
+    public int setBitCount(int n){
+        int setBits = 0;
+        while(n > 0){
+            if((n & 1) != 0){
+                setBits++;
+            }
 
-    public boolean isSet(int x, int bit) {
-        return (x & (1 << bit)) != 0;
+            n = n>>1;
+        }
+
+        return setBits;
     }
-
-    public int setBit(int x, int bit) {
-        return x | (1 << bit);
-    }
-
-    public int unsetBit(int x, int bit) {
-        return x & ~(1 << bit);
-    }
-
-    public boolean isUnSet(int x, int bit) {
-        return (x & (1 << bit)) == 0;
-    }
-
     public int minimizeXor(int num1, int num2) {
-        int x = 0;
 
-        int requiredSetBitCount = Integer.bitCount(num2);
-        
-        for(int bit = 31; bit >= 0 && requiredSetBitCount > 0; bit--) {
-            if(isSet(num1, bit)) {
-                x |= (1 << bit);
-                requiredSetBitCount--;
+        int setBits2 = setBitCount(num2);
+        int setBits1 = setBitCount(num1);
+        int x = num1;
+
+        if(setBits1 < setBits2){
+            int bit = 0;
+            for(int i = 0; i < (setBits2 - setBits1); i++){
+                while((x & (1<<bit)) != 0){
+                    bit++;
+                }
+                x = x | (1<<bit);
             }
         }
-
-        for(int bit = 0; bit < 32 && requiredSetBitCount > 0; bit++) {
-            if(isUnSet(num1, bit)) {
-                x |= (1 << bit);
-                requiredSetBitCount--;
+        else if(setBits1 > setBits2){
+            int bit = 0;
+            for(int i = 0; i < (setBits1 - setBits2); i++){
+                while((x & (1<<bit)) == 0){
+                    bit++;
+                }
+                x = x & ~(1<<bit);
             }
         }
         
+
         return x;
+        
     }
 }
