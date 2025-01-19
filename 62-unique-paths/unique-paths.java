0fa93eecdb22ebece2f35c6b5e39d6f75101467d
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-19 13:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (15.71%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,28 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
 
-        
-        for(int i = m - 1; i >= 0; i--){
-            for(int j = n - 1; j >= 0; j--){
-                if((i == m - 1) && (j == n - 1)){
-                    dp[m - 1][n - 1] = 1;
-                    continue;
-                }
-                int right = 0;
-                int down = 0;
-                if(i < m - 1){
-                    down = dp[i + 1][j];
+        int[] dp = new int[n]; 
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                // base case
+                if (i == m - 1 || j == n - 1) {
+                    dp[n - 1] = 1;
                 }
 
-                if(j < n - 1){
-                    right = dp[i][j + 1];
+         
+                if (j < n - 1) {
+                    dp[j] += dp[j + 1];
+                    //     here previous dp[j] is last column ans(j) for last row and dp[j + 1] is the column answer calculated on previous iteration
                 }
-
-                dp[i][j] = right + down;
             }
         }
 
-        return dp[0][0];
+        return dp[0];
+
     }
 }
