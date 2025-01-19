f56d55e66b2e847782bc9f7baff3fb38e4ddb70b
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-19 13:23:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (56.60%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,37 +1,28 @@
 class Solution {
-    public int helper(int m, int n, int i, int j, int[][] dp) {
-        // base case
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-
-        // momoized
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int right = 0;
-        if ((i + 1) < m) {
-            right = helper(m, n, i + 1, j, dp);
-        }
-
-        int down = 0;
-        if ((j + 1) < n) {
-            down = helper(m, n, i, j + 1, dp);
-        }
-
-        dp[i][j] = right + down;
-
-        return dp[i][j];
-    }
-
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
 
-        // filling 2D array with -1
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if((i == m - 1) && (j == n - 1)){
+                    dp[m - 1][n - 1] = 1;
+                    continue;
+                }
+                int right = 0;
+                int down = 0;
+                if(i < m - 1){
+                    down = dp[i + 1][j];
+                }
+
+                if(j < n - 1){
+                    right = dp[i][j + 1];
+                }
+
+                dp[i][j] = right + down;
+            }
         }
-        return helper(m, n, 0, 0, dp);
+
+        return dp[0][0];
     }
 }
