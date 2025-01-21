64e883503b10516ffd29446fad385da1b92b0ec3
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-21 09:33:04 +0530

    Time: 36 ms (25.96%) | Memory: 68.6 MB (47.60%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,46 +1,45 @@
 class Solution {
-    private boolean checkRowPainted(int[][] mat, int row) {
-        for (int col = 0; col < mat[0].length; col++) {
-            if (mat[row][col] > 0) { // not painted
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkColPainted(int[][] mat, int col) {
-        for (int row = 0; row < mat.length; row++) {
-            if (mat[row][col] > 0) { // not painted
-                return false;
-            }
-        }
-        return true;
-    }
-
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
+        int len = m * n;
 
-        Map<Integer, int[]> mp = new HashMap<>(); // stores value to cell-coordinate [i][j]
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int val = mat[i][j];
-                mp.put(val, new int[]{i, j});
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < len; i++){
             int val = arr[i];
-            int[] coordinates = mp.get(val);
-            int row = coordinates[0], col = coordinates[1];
-            mat[row][col] *= -1; // painted
 
-            if (checkRowPainted(mat, row) || checkColPainted(mat, col)) {
-                return i;
-            }
+            map.put(val, i);
         }
 
-        return -1;
+
+        int minIndex = Integer.MAX_VALUE;
+        for(int i = 0; i < m; i++){
+
+            int lastIndex = Integer.MIN_VALUE;
+            for(int j = 0; j < n; j++){
+                int val = mat[i][j];
+
+                int index = map.get(val);
+                lastIndex = Math.max(lastIndex, index);
+            }
+
+            minIndex = Math.min(minIndex, lastIndex);
+        }
+
+        for(int i = 0; i < n; i++){
+
+            int lastIndex = Integer.MIN_VALUE;
+            for(int j = 0; j < m; j++){
+                int val = mat[j][i];
+
+                int index = map.get(val);
+                lastIndex = Math.max(lastIndex, index);
+            }
+
+            minIndex = Math.min(minIndex, lastIndex);
+        }
+
+        return minIndex;
     }
 }
