960ac51388f971983cfdf514da33a16a315686ad
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-23 10:07:43 +0530

    Time: 48 ms (75.05%) | Memory: 168 MB (34.20%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.java
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -1,38 +1,42 @@
 class Solution {
     public int[][] highestPeak(int[][] isWater) {
-        int dir[][] = {{-1,0},{0,1},{1,0},{0,-1}};
-        //fill the queue and mark res as negative
-        int n = isWater.length;
-        int m = isWater[0].length;
-        int res[][] = new int[n][m];
-        //[r,c]
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int m = isWater.length;
+        int n = isWater[0].length;
+
+        int[][] res = new int[m][n];
+
         Queue<int[]> queue = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(isWater[i][j] == 1){
                     res[i][j] = 0;
-                    queue.offer(new int[]{i,j});
-                }else{
+                    queue.offer(new int[]{i, j}); // d
+                }
+                else{
                     res[i][j] = -1;
                 }
             }
         }
+
         while(!queue.isEmpty()){
-            int cell[] = queue.poll();
+            int[] cell = queue.poll();
             int r = cell[0];
             int c = cell[1];
             int h = res[r][c];
-            for(int i=0;i<4;i++){
+
+            for(int i = 0; i < 4; i++){
                 int nr = r + dir[i][0];
                 int nc = c + dir[i][1];
-                if(nr>=0 && nr<n
-                && nc>=0 && nc<m
-                && res[nr][nc] == -1){
-                    res[nr][nc] = h+1; //visit, height++
-                    queue.offer(new int[]{nr,nc});
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && (res[nr][nc] == -1)){
+                    res[nr][nc] = h + 1;
+                    queue.offer(new int[]{nr, nc});
                 }
             }
         }
+
         return res;
     }
 }
