af596e181cf3d55e109057726857545680c6e21c
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-01-28 15:49:00 +0530

    Time: 3 ms (100.00%) | Memory: 45 MB (71.43%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,32 +1,31 @@
 class Solution {
+    int rows;
+    int cols;
     public int findMaxFish(int[][] grid) {
-        int row = grid.length;
-        int cols = grid[0].length;
-        boolean[][] visited = new boolean[row][cols];
-        int ans = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] > 0 && (!visited[i][j])) {
-                    ans = Math.max(ans, dfs(i, j, grid, visited));
-                    visited[i][j] = true;
+        rows = grid.length;
+        cols = grid[0].length;
+        int maxFishes=0;
+        //find max of all components
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]!=0){
+                    maxFishes=Math.max(maxFishes, dfs(i,j,grid));
                 }
             }
         }
-
-        return ans;
+        return maxFishes;
     }
-
-    public int dfs(int r, int c, int[][] grid, boolean[][] visited) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||  visited[r][c] || grid[r][c] == 0) {
+    public int dfs(int r, int c, int grid[][]){
+        //base case
+        //out of the matrix
+        //land cell -> 0
+        //visited ->0
+        if(r<0 || r>=rows || c<0 || c>= cols || grid[r][c] == 0){
             return 0;
         }
-        visited[r][c] = true;
-        int sum = grid[r][c];
-        sum += dfs(r + 1, c, grid, visited);
-        sum += dfs(r - 1, c, grid, visited);
-        sum += dfs(r, c + 1, grid, visited);
-        sum += dfs(r, c - 1, grid, visited);
-        return sum;
+        int ans = grid[r][c];
+        grid[r][c] = 0;
+        ans += dfs(r-1,c,grid) + dfs(r,c+1,grid) + dfs(r+1,c,grid) + dfs(r,c-1,grid);
+        return ans;
     }
 }
