9d501fb47ca8d65bd3c1a08e250f9dd567f6acb9
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-02-02 02:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (84.29%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -5,39 +5,10 @@ class Solution {
             return true;
         }
 
-        if(len == 2){
-            if(nums[0] == nums[1]){
-                return false;
-            }
-            if(nums[0] % 2 == 0 && nums[1] % 2 != 0){
-                return true;
-            }
-            else if(nums[0] % 2 != 0 && nums[1] % 2 == 0){
-                return true;
-            }
-            else{
-                return false;
-            }
+        for(int i = 0; i < len - 1; i++){
+            if(nums[i] % 2 == nums[i + 1] % 2) return false;
         }
 
-int count = 0;
-
-        for(int i = 1; i < len - 1; i++){
-            int val = nums[i];
-            if(val == nums[i - 1] || val == nums[i + 1]){
-                return false;
-            }
-            if(val % 2 != 0 && nums[i - 1] % 2 == 0 && nums[i + 1] % 2 == 0 || val % 2 == 0 && nums[i - 1] % 2 != 0 && nums[i + 1] % 2 != 0){
-                count++;
-            }
-            
-        }
-
-        if(count == len - 2){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 }
