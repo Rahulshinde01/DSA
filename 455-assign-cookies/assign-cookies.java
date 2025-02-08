d968f80909db03968df794ee327bd668a050e246
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-02-08 17:08:47 +0530

    Time: 11 ms (42.10%) | Memory: 48.6 MB (57.71%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int ans = 0;
+        int n1 = g.length;
+        int n2 = s.length;
+
+        int i = 0; 
+        int j = 0;
+
+        while(i < n1 && j < n2){
+            if(g[i] <= s[j]){
+                ans++;
+                i++;
+            }
+            j++;
+        }
+
+        return ans;
+    }
+}
