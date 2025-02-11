eaf0f3fb67c2799ddb70fc55e35ab5dfe4092073
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-02-12 00:23:46 +0530

    Time: 30 ms (19.44%) | Memory: 45.5 MB (70.00%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -3,27 +3,22 @@ class Solution {
         Stack<Character> stack=new Stack<>();
         int count=0;
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c == '('){
-                stack.push(c);
-            }
-            else{
+// -Keep adding left barckets
+            if(s.charAt(i)=='('){
+                stack.push(s.charAt(i));
+            }else{
+                // for closing baracket without stack is empty
                 if(i+1<s.length()&& s.charAt(i+1)==')'){
+                    // -Skip it as two consecutives are present
                     i++;
-                }
-                else {
-                    count++;
-                }
+                }else count++;
                 
                 if(!stack.isEmpty()){
                     stack.pop();
-                }
-                else{
-                    count++;
-                }
+                }else count++ ; //// for closing baracket with stack is empty
             }
         }
-        count += 2*stack.size();
+        count+=2*stack.size();
         return count;
     }
 }
